d9bbd6043b3015f1b6194374a351aec5b622e663
vasco <vastur@gmail.com>
2023-08-05 21:16:23 +0100

    Fix #5. Publish ES module files to NPM.

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 
 import pkg from './package.json' assert { type: 'json' };
-const { name, homepage, version } = pkg;
+const { name, homepage, version, dependencies } = pkg;
 
 const umdConf = {
   format: 'umd',
@@ -13,25 +13,40 @@ const umdConf = {
   banner: `// Version ${version} ${name} - ${homepage}`
 };
 
-export default {
-  input: 'src/index.js',
-  output: [
-    { // umd
-      ...umdConf,
-      file: `dist/${name}.js`,
-      sourcemap: true
-    },
-    { // minify
-      ...umdConf,
-      file: `dist/${name}.min.js`,
-      plugins: [terser({
-        output: { comments: '/Version/' }
-      })]
-    }
-  ],
-  plugins: [
-    babel({ exclude: 'node_modules/**' }),
-    resolve(),
-    commonJs()
-  ]
-};
+export default [
+  { // UMD
+    input: 'src/index.js',
+    output: [
+      {
+        ...umdConf,
+        file: `dist/${name}.js`,
+        sourcemap: true
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
+    ],
+    plugins: [
+      babel({ exclude: 'node_modules/**' }),
+      resolve(),
+      commonJs()
+    ]
+  },
+  { // ES module
+    input: 'src/index.js',
+    output: [
+      {
+        format: 'es',
+        file: `dist/${name}.mjs`
+      }
+    ],
+    external: Object.keys(dependencies),
+    plugins: [
+      babel()
+    ]
+  },
+];
